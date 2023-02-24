1ef5be8057e61890505748d24a637ce58469578a
john <john@121.168.203.19>
2023-02-25 02:01:36 +0900

    일단 해봄

--- hongpark/src/main/java/com/example/hongpark/objectmapper/Burger.java
+++ hongpark/src/main/java/com/example/hongpark/objectmapper/Burger.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 @Getter
 public class Burger {
--- hongpark/src/test/java/com/example/hongpark/objectmapper/BurgerTest.java
+++ hongpark/src/test/java/com/example/hongpark/objectmapper/BurgerTest.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,10 +23,31 @@ public class BurgerTest {
 		List<String> ingredients = Arrays.asList("통새우패티", "순쇠고기패티", "토마토", "양상추", "치즈");
 		Burger burger = new Burger("맥날 슈비", 5500, ingredients);
 		
-		String actual = om.writeValueAsString(burger);
+		String actual = om.writeValueAsString(burger); //자바객체 -> json
 		
 		String expected = "{\"name\":\"맥날 슈비\",\"price\":5500,\"ingredients\":[\"통새우패티\",\"순쇠고기패티\",\"토마토\",\"양상추\",\"치즈\"]}";
 		assertEquals(expected, actual);
-		log.info(actual);
+		
+		//json 이쁘게 출력
+		JsonNode jsonNode = om.readTree(actual);
+		log.info(jsonNode.toPrettyString());
+	}
+	
+	@Test
+	public void JSON_자바객체로() throws JsonMappingException, JsonProcessingException {
+		ObjectMapper om = new ObjectMapper();
+		String jsonStr = "{\"name\":\"맥날 슈비\",\"price\":5500,\"ingredients\":[\"통새우패티\",\"순쇠고기패티\",\"토마토\",\"양상추\",\"치즈\"]}";
+		
+		Burger actual = om.readValue(jsonStr, Burger.class); //json -> 자바객체
+		List<String> ingredients = Arrays.asList("통새우패티", "순쇠고기패티", "토마토", "양상추", "치즈");
+		Burger expected = new Burger("맥날 슈비", 5500, ingredients);
+		
+		assertEquals(expected.toString(), actual.toString());
+		
+		//json 이쁘게 출력
+		JsonNode jsonNode = om.readTree(jsonStr);
+		log.info(jsonNode.toPrettyString());
+		log.info(actual.toString());
+
 	}
 }
