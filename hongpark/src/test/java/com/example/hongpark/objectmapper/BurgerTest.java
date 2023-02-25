420038f43453903fb1d4279bc68c754e382d043a
john <john@121.168.203.19>
2023-02-25 21:24:49 +0900

    31강 objectmapper

--- hongpark/src/test/java/com/example/hongpark/objectmapper/BurgerTest.java
+++ hongpark/src/test/java/com/example/hongpark/objectmapper/BurgerTest.java
@@ -11,6 +11,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,36 +20,91 @@ import lombok.extern.slf4j.Slf4j;
 public class BurgerTest {
 	
 	@Test
-	public void 자바객체_JSON으로() throws JsonProcessingException {
-		ObjectMapper om = new ObjectMapper();
+	public void javaobj_to_JSONstr() throws JsonProcessingException {
+		
+		//자바객체
 		List<String> ingredients = Arrays.asList("통새우패티", "순쇠고기패티", "토마토", "양상추", "치즈");
-		Burger burger = new Burger("맥날 슈비", 5500, ingredients);
+		Burger burger = new Burger("맥날 슈비", 5500, ingredients);		
+
+		//자바객체에서 만들어진 실제 jsonstr
+		ObjectMapper om = new ObjectMapper();
+		String actual = om.writeValueAsString(burger); //자바객체 -> jsonstr
 		
-		String actual = om.writeValueAsString(burger); //자바객체 -> json
-		
+		//예상 jsonstr
 		String expected = "{\"name\":\"맥날 슈비\",\"price\":5500,\"ingredients\":[\"통새우패티\",\"순쇠고기패티\",\"토마토\",\"양상추\",\"치즈\"]}";
+		
 		assertEquals(expected, actual);
 		
-		//json 이쁘게 출력
+		//실제 jsonstr 이쁘게 출력
 		JsonNode jsonNode = om.readTree(actual);
+		log.info(burger.toString());
 		log.info(jsonNode.toPrettyString());
 	}
 	
 	@Test
-	public void JSON_자바객체로() throws JsonMappingException, JsonProcessingException {
-		ObjectMapper om = new ObjectMapper();
+	public void JSONstr_to_javaobj() throws JsonMappingException, JsonProcessingException {
+
+		//jsonstr
 		String jsonStr = "{\"name\":\"맥날 슈비\",\"price\":5500,\"ingredients\":[\"통새우패티\",\"순쇠고기패티\",\"토마토\",\"양상추\",\"치즈\"]}";
+
+		//jsonstr에서 만들어진 실제 자바 객체
+		ObjectMapper om = new ObjectMapper();
+		Burger actual = om.readValue(jsonStr, Burger.class); //jsonstr -> 자바객체
 		
-		Burger actual = om.readValue(jsonStr, Burger.class); //json -> 자바객체
+		//예상 자바 객체
 		List<String> ingredients = Arrays.asList("통새우패티", "순쇠고기패티", "토마토", "양상추", "치즈");
 		Burger expected = new Burger("맥날 슈비", 5500, ingredients);
 		
 		assertEquals(expected.toString(), actual.toString());
 		
-		//json 이쁘게 출력
+		//jsonstr 이쁘게 출력
+		//jsonstr -> jsonobj -> prettystr
 		JsonNode jsonNode = om.readTree(jsonStr);
-		log.info(jsonNode.toPrettyString());
-		log.info(actual.toString());
+		log.info(jsonNode.toPrettyString()); //jsonstr
+		log.info(actual.toString()); //실제 자바객체
 
 	}
+	
+	@Test
+	public void JSONobj_to_javaobj() throws JsonProcessingException {
+		
+		//jsonobj 생성
+		ObjectMapper om = new ObjectMapper();
+		ObjectNode objectNode = om.createObjectNode();
+		objectNode.put("name", "맥날 슈비");
+		objectNode.put("price", "5500");
+
+		//배열을 넣으려면
+		ArrayNode arrayNode = om.createArrayNode();
+		arrayNode.add("통새우패티");
+		arrayNode.add("순쇠고기패티");
+		arrayNode.add("토마토");
+		arrayNode.add("양상추");
+		arrayNode.add("치즈");
+		
+		//objectNode.put("ingredients", arrayNode); put은 deprecated
+		objectNode.set("ingredients", arrayNode); // set 사용
+		
+		//실제
+		String jsonStr = objectNode.toString(); //jsonobj -> jsonstr
+		Burger actual = om.readValue(jsonStr, Burger.class); //jsonstr -> 자바객체
+		
+		
+		//예상 자바 객체
+		List<String> ingredients = Arrays.asList("통새우패티", "순쇠고기패티", "토마토", "양상추", "치즈");
+		Burger expected = new Burger("맥날 슈비", 5500, ingredients);
+
+		assertEquals(expected.toString(), actual.toString());
+		
+		//jsonstr 이쁘게 출력
+		JsonNode jsonNode = om.readTree(jsonStr);
+		log.info(jsonNode.toPrettyString()); //jsonstr -> jsonobj -> prettystr 
+		log.info(objectNode.toPrettyString());  //직접 objectNode 사용
+		log.info(actual.toString()); //실제 자바객체
+	}
+	
+	//정리
+	//objectNode -> .toString() -> jsonStr
+	//jsonStr -> ObjectMapper.readValue(jsonStr, .class) -> javaobj
+	//javaobj -> ObjectMapper.writeValueAsString(javaobj) -> jsonStr
 }
